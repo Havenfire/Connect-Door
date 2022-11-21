2bee6bc9b642829fd25afd3b955be43e743005c7
Randy Carlson <randycarlson486@gmail.com>
2022-11-20 20:52:09 -0800

    refactored into Board class

--- connect_4.py
+++ connect_4.py
@@ -1,4 +1,5 @@
-
+import copy
+from functools import cached_property
 
 from agents import *
 from constants import *
@@ -6,15 +7,78 @@ from utils import *
 
 
 def initialize():
-    board = []
-    for i in range(SIZE_Y):
-        board.append([CHAR_EMPTY] * SIZE_X)
+    board = Board(SIZE_X, SIZE_Y, CHAR_EMPTY)
 
     return board
 
 
+class Board:
+    def __init__(self, size_x, size_y, fill):
+        self.size_x = size_x
+        self.size_y = size_y
+        
+        self.board = []
+        for _ in range(size_y):
+            self.board.append([fill] * size_x)
+
+    @cached_property
+    def win_lines(self):
+        lines = []
+        # column
+        for x in range(SIZE_X):
+            for y in range(SIZE_Y - FOUR + 1):
+                lines.append(tuple((x, y + i) for i in range(FOUR)))
+        # row
+        for x in range(SIZE_X - FOUR + 1):
+            for y in range(SIZE_Y):
+                lines.append(tuple((x + i, y) for i in range(FOUR)))
+        # diag SE
+        for x in range(SIZE_X - FOUR + 1):
+            for y in range(SIZE_Y - FOUR + 1):
+                lines.append(tuple((x + i, y + i) for i in range(FOUR)))
+        # diag SW
+        for x in range(FOUR - 1, SIZE_X):
+            for y in range(SIZE_Y - FOUR + 1):
+                lines.append(tuple((x - i, y + i) for i in range(FOUR)))
+        
+        return lines
+
+    def get_wins(self):
+        wins = []
+        for line in self.win_lines:
+            char = board[line[0][1]][line[0][0]]
+            if char == CHAR_EMPTY:
+                continue
+            same = True
+            for spot in line:
+                if board[spot[1]][spot[0]] != char:
+                    same = False
+                    break
+            if same:
+                wins.append((char, line))
+
+        return sorted(wins)
+
+    def check_win(self):
+        wins = self.get_wins()
+        return None if len(wins) == 0 else wins[0][0]
+
+
+    def __setitem__(self, index, value):
+        self.board.__setitem__(index, value)
+
+    def __getitem__(self, index):
+        return self.board.__getitem__(index)
+
+    def print(self):
+        print('\n'.join((' '.join(r) for r in self.board)))
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+
 def main_loop(board):
-    players = [Agent(CHAR_0), Human(CHAR_1)]
+    players = [Agent(CHAR_0), Agent(CHAR_1)]
     for i in range(SIZE_X * SIZE_Y):
         curr_player = players[i % len(players)]
         print(f'turn {i}, {curr_player}')
@@ -26,8 +90,8 @@ def main_loop(board):
                 break
         board[y][col] = curr_player.char
         print(f'placed at {y} {col}')
-        print_board(board)
-        winner = check_win(board)
+        board.print()
+        winner = board.check_win()
         if winner:
             print(f'{winner} has won!')
             break
--- utils.py
+++ utils.py
@@ -1,60 +1,12 @@
+import sys
+
 from constants import *
 
 def print_board(board):
-    print('\n'.join((' '.join(r) for r in board)))
+    print('utils.print_board(board) is deprecated, please use board.print() instead.', file=sys.stderr)
+    return board.print()
 
 
 def check_win(board):
-    # column
-    for x in range(SIZE_X):
-        for y in range(SIZE_Y - FOUR + 1):
-            if board[y][x] == CHAR_EMPTY:
-                continue
-            same = True
-            for i in range(1, FOUR):
-                if board[y + i][x] != board[y][x]:
-                    same = False
-                    break
-            if same:
-                return board[y][x]
-
-    # row
-    for x in range(SIZE_X - FOUR + 1):
-        for y in range(SIZE_Y):
-            if board[y][x] == CHAR_EMPTY:
-                continue
-            same = True
-            for i in range(1, FOUR):
-                if board[y][x + i] != board[y][x]:
-                    same = False
-                    break
-            if same:
-                return board[y][x]
-
-    # diag SE
-    for x in range(SIZE_X - FOUR + 1):
-        for y in range(SIZE_Y - FOUR + 1):
-            if board[y][x] == CHAR_EMPTY:
-                continue
-            same = True
-            for i in range(1, FOUR):
-                if board[y + i][x + i] != board[y][x]:
-                    same = False
-                    break
-            if same:
-                return board[y][x]
-
-    # diag SW
-    for x in range(FOUR - 1, SIZE_X):
-        for y in range(SIZE_Y - FOUR + 1):
-            if board[y][x] == CHAR_EMPTY:
-                continue
-            same = True
-            for i in range(1, FOUR):
-                if board[y + i][x - i] != board[y][x]:
-                    same = False
-                    break
-            if same:
-                return board[y][x]
-
-    return None
+    print('utils.check_win(board) is deprecated, please use board.check_win() instead.', file=sys.stderr)
+    return board.check_win()
